92460f46bdc0c4ee846aaa6e59dfe7840f0f3de1
xiaopan1991 <xiaopan126@126.com>
2015-11-02 00:04:50 +0800

    mailmenu + photon
    提交mailmenu界面＋添加photon

---
+++ Assets/Script/photon/PhotonEngine.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+using ExitGames.Client.Photon;
+using System.Collections.Generic;
+using GameCommon;
+
+public class PhotonEngine : MonoBehaviour,IPhotonPeerListener
+{
+	public ConnectionProtocol protocol = ConnectionProtocol.Tcp;
+	public string serverAddress = "127.0.0.1:4530";
+	public string applicationName = "GameServer";
+
+	private PhotonPeer peer;
+	private bool isConnected = false;
+	private static PhotonEngine _instance;
+	private Dictionary<byte, ControllerBase> controllers = new Dictionary<byte, ControllerBase>();
+
+	public static PhotonEngine Instance
+	{
+		get{return _instance;}
+	}
+
+	void Awake () {
+		peer = new PhotonPeer(this, protocol);
+		peer.Connect(serverAddress, applicationName);
+		peer.Service();
+	}
+
+	void Update () {
+		if(isConnected)
+		{
+			peer.Service();
+		}
+	}
+
+	public void RegisterController(OperationCode opCode, ControllerBase controller)
+	{
+		controllers.Add((byte)opCode, controller);
+	}
+
+	public void DebugReturn (DebugLevel level, string message)
+	{
+		print("level: " + level + "|||" + "message: " + message);
+	}
+	public void OnOperationResponse (OperationResponse operationResponse)
+	{
+		ControllerBase controller;
+		controllers.TryGetValue(operationResponse.OperationCode, out controller);
+		if(controller != null)
+		{
+			controller.OnOperationResponse(operationResponse);
+		}
+		else
+		{
+			print("Received a unknow response. OperationCode: " + operationResponse.OperationCode);
+		}
+	}
+	public void OnStatusChanged (StatusCode statusCode)
+	{
+		Debug.Log("OnStatusChange: " + statusCode);
+		switch(statusCode)
+		{
+		case StatusCode.Connect:
+			isConnected = true;
+			break;
+		default:
+			isConnected = false;
+			break;
+		}
+	}
+	public void OnEvent (EventData eventData)
+	{
+		throw new System.NotImplementedException ();
+	}
+}
---
+++ Assets/Script/photon/controller/ControllerBase.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using GameCommon;
+using ExitGames.Client.Photon;
+
+public abstract class ControllerBase : MonoBehaviour {
+
+	public OperationCode opCode;
+
+	void Start()
+	{
+		PhotonEngine.Instance.RegisterController(opCode, this);
+	}
+
+	public abstract void OnOperationResponse(OperationResponse response);
+
+}
