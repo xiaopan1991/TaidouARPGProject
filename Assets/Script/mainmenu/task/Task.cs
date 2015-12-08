5f64baf91bd86d78636e5d6cb9509943db066a7a
xiaopan1991 <xiaopan126@126.com>
2015-12-08 23:47:54 +0800

    任务系统
    开始制作任务系统

---
+++ Assets/Script/mainmenu/task/Task.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum TaskType
+{
+	Main,
+	Reward,
+	Daily
+}
+
+public enum TaskProgress
+{
+	NoStart,
+	Accept,
+	Complete,
+	Reward
+}
+
+public class Task 
+{
+	private int id;
+	public int Id
+	{
+		get{return id;}
+		set{id = value;}
+	}
+
+	private string name;
+	public string Name
+	{
+		get{return name;}
+		set{name = value;}
+	}
+
+	private TaskType taskType;
+	public TaskType TaskType
+	{
+		get{return taskType;}
+		set{taskType = value;}
+	}
+	private string icon;
+	public string Icon
+	{
+		get{return icon;}
+		set{icon = value;}
+	}
+	private string des;
+	public string Des
+	{
+		get{return des;}
+		set{des = value;}
+	}
+	private int coin;
+	public int Coin
+	{
+		get{return coin;}
+		set{coin=value;}
+	}
+	private int diamond;
+	public int Diamond
+	{
+		get{return diamond;}
+		set{diamond = value;}
+	}
+	private string talkNpc;
+	public string TalkNpc
+	{
+		get{return talkNpc;}
+		set{talkNpc = value;}
+	}
+	private int idNpc;
+	public int IdNpc
+	{
+		get{return idNpc;}
+		set{idNpc = value;}
+	}
+	private int idTranscript;
+	public int IdTranscript
+	{
+		get{return idTranscript;}
+		set{idTranscript = value;}
+	}
+	private TaskProgress taskProgress = TaskProgress.NoStart;
+	public TaskProgress TaskProgress
+	{
+		get{return taskProgress;}
+		set{taskProgress = value;}
+	}
+}
