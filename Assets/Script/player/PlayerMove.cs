73095c1ff891b225a0c95c24d36aa13879dff289
xiaopan1991 <xiaopan126@126.com>
2015-12-12 00:24:09 +0800

    添加人物
    场景中添加人物

---
+++ Assets/Script/player/PlayerMove.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : MonoBehaviour {
+
+	public float velocity = 5;
+	private Rigidbody m_rigidbody;
+
+
+	void Awake()
+	{
+		this.m_rigidbody = GetComponent<Rigidbody>();
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float h = Input.GetAxis("Horizontal");
+		float v = Input.GetAxis("Vertical");
+		Vector3 vel = m_rigidbody.velocity;
+		this.m_rigidbody.velocity = new Vector3(-h*velocity, vel.y, -v*velocity);
+	}
+}
