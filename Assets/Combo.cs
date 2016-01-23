610d0e2ea6f19ef3e34564c60fd65662699bcf0a
xiaopan1991 <xiaopan126@126.com>
2016-01-23 18:50:21 +0800

    添加伤害数值与血条

---
+++ Assets/Combo.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Combo : MonoBehaviour {
+
+	public static Combo _instance;
+	public float comboTime = 2;
+	private int comboCount = 0;
+	private float timer = 0;
+	private UILabel numberLabel;
+
+	void Awake()
+	{
+		_instance = this;
+		this.gameObject.SetActive(false);
+		numberLabel = transform.Find("NumberLabel").GetComponent<UILabel>();
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer -= Time.deltaTime;
+		if(timer <= 0)
+		{
+			this.gameObject.SetActive(false);
+			comboCount = 0;
+		}
+	}
+
+	public void ComboPlus()
+	{
+		this.gameObject.SetActive(true);
+		timer = comboTime;
+		comboCount++;
+		numberLabel.text = comboCount.ToString();
+		transform.localScale = Vector3.one;
+		iTween.ScaleTo(this.gameObject, new Vector3(1.5f,1.5f,1.5f), 0.1f);
+		iTween.ShakePosition(this.gameObject, new Vector3(0.15f,0.15f,0.15f), 0.15f);
+	}
+}
--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -5,19 +5,101 @@ public class Enemy : MonoBehaviour {
 
 	public GameObject damageEffectPrefab;
 	public int hp = 200;
+	public float speed = 2;
+	public int attackRate = 2;
+	public float attackDistance = 2;
+	public float downSpeed = 2.0f;
+	private float attackTimer = 0;
+	private float distance = 0;
+	private float downDistance = 0;
+	private int hpTotal = 0;
 
-	private Animation eneryAnimation;
+	private Animation enemyAnimation;
 	private Transform bloodPoint;
+	private CharacterController cc;
+	private GameObject hpBarGameObject;
+	private UISlider hpBarSlider;
+	private GameObject hudTextGameObject;
+	private HUDText hudText;
 
 	// Use this for initialization
 	void Awake () {
-		eneryAnimation = this.GetComponent<Animation>();
+		enemyAnimation = this.GetComponent<Animation>();
 		bloodPoint = transform.Find("BloodPoint");
+		cc = this.GetComponent<CharacterController>();
+
+		Transform hpBarPoint = transform.Find("HpBarPoint");
+		hpBarGameObject = HpBarManager._instance.GetHpBar(hpBarPoint.gameObject);
+		hpBarSlider = hpBarGameObject.transform.Find("Bg").GetComponent<UISlider>();
+		hudTextGameObject = HpBarManager._instance.GetHudText(hpBarPoint.gameObject);
+		hudText = hudTextGameObject.GetComponent<HUDText>();
+
+		hpTotal = hp;
+	}
+
+	void Start()
+	{
+		InvokeRepeating("CalcDistance", 0, 0.1f);
+
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if(hp <=0 ) 
+		{
+			downDistance += downSpeed * Time.deltaTime;
+			transform.Translate(-transform.up * downSpeed * Time.deltaTime);
+			if(downDistance > 4)
+			{
+				Destroy(this.gameObject);
+			}
+			return;
+		}
+		if(distance < attackDistance)
+		{
+			attackTimer += Time.deltaTime;
+			if(attackTimer > attackRate)
+			{
+				Transform player = TranscriptManager._instance.Player.transform;
+				Vector3 targetPos = player.position;
+				targetPos.y = transform.position.y;
+				transform.LookAt(targetPos);
+				enemyAnimation.Play("attack01");
+				attackTimer = 0;
+			}
+			if(!enemyAnimation.IsPlaying("attack01"))
+			{
+				enemyAnimation.CrossFade("idle");
+			}
+		}
+		else
+		{
+			enemyAnimation.Play("walk");
+			Move();
+		}
+	}
+
+	void CalcDistance()
+	{
+		Transform player = TranscriptManager._instance.Player.transform;
+		distance = Vector3.Distance(player.position, transform.position);
+	}
+
+	void Move()
+	{
+		Transform player = TranscriptManager._instance.Player.transform;
+		Vector3 targetPos = player.position;
+		targetPos.y = transform.position.y;
+		transform.LookAt(targetPos);
+		cc.SimpleMove(transform.forward * speed);
+	}
+
+	void Dead()
+	{
+		Destroy(hpBarGameObject);
+		Destroy(hudTextGameObject);
+		enemyAnimation.Play("die");
 	}
 
 	//受到攻击
@@ -25,25 +107,27 @@ public class Enemy : MonoBehaviour {
 	void TakeDamage(string arg)
 	{
 		if(hp <= 0) return;
-		
+		Combo._instance.ComboPlus();
 		string[] proArray = arg.Split(',');
 
 		int damage = int.Parse(proArray[0]);
 		hp -= damage;
+		hpBarSlider.value = (float)hp/hpTotal;
+		hudText.Add("-"+damage, Color.red, 0.3f);
 		if(hp <= 0)
 		{
 			Dead();
 		}
 
-		eneryAnimation.Play("takedamage");
+		enemyAnimation.Play("takedamage");
 		float backDistance = float.Parse(proArray[1]);
 		float jumpHeight = float.Parse(proArray[2]);
 		iTween.MoveBy(this.gameObject, transform.InverseTransformDirection(TranscriptManager._instance.Player.transform.forward)*backDistance + Vector3.up*jumpHeight,0.3f);
 		GameObject.Instantiate(damageEffectPrefab, bloodPoint.position, Quaternion.identity);
-	}
 
-	void Dead()
-	{
-		
+		if(hp <=0 ) 
+		{
+			Dead();
+		}
 	}
 }
